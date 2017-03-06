64fbefd8d3e4a9f1a9178c28d24a37c75ba7e63f
Atin Mathur <atin@MacBook.local>
2017-03-06 17:09:49 +0530

    Added copy

--- public/bundle.js
+++ public/bundle.js
@@ -26807,9 +26807,18 @@
 	// Stateless functional component
 	var Examples = function Examples(props) {
 	  return React.createElement(
-	    'h3',
+	    'div',
 	    null,
-	    'Examples component'
+	    React.createElement(
+	      'h3',
+	      null,
+	      'Examples component'
+	    ),
+	    React.createElement(
+	      'p',
+	      null,
+	      'Welcome to examples page'
+	    )
 	  );
 	};
 
