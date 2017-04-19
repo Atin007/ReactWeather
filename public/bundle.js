90826ffe07f4026e9b09d4b1a9b8e863f87bfccf
Atin Mathur <atin.mathur.10@gmail.com>
2017-04-19 19:54:45 +0530

    Add styles to weather page

--- public/bundle.js
+++ public/bundle.js
@@ -25550,7 +25550,7 @@
 	          React.createElement(
 	            'li',
 	            { className: 'menu-text' },
-	            'ReactWeather'
+	            'React Weather'
 	          ),
 	          React.createElement(
 	            'li',
@@ -25656,7 +25656,7 @@
 	      if (isLoading) {
 	        return React.createElement(
 	          'h3',
-	          null,
+	          { className: 'text-center' },
 	          'Fetching Weather...'
 	        );
 	      } else if (temp && location) {
@@ -25668,9 +25668,9 @@
 	      'div',
 	      null,
 	      React.createElement(
-	        'h3',
-	        null,
-	        'Weather components'
+	        'h1',
+	        { className: 'text-center' },
+	        'Get Weather'
 	      ),
 	      React.createElement(WeatherForm, { onSearch: this.handleSearch }),
 	      renderMessage()
@@ -25709,11 +25709,9 @@
 	        'form',
 	        { onSubmit: this.onFormSubmit },
 	        React.createElement('input', { type: 'text', ref: 'location' }),
-	        React.createElement('br', null),
-	        React.createElement('br', null),
 	        React.createElement(
 	          'button',
-	          null,
+	          { className: 'button expanded hollow' },
 	          'Get Weather'
 	        )
 	      )
@@ -25727,7 +25725,7 @@
 /* 233 */
 /***/ function(module, exports, __webpack_require__) {
 
-	'use strict';
+	"use strict";
 
 	var React = __webpack_require__(8);
 
@@ -25747,11 +25745,11 @@
 	      location = _ref.location;
 
 	  return React.createElement(
-	    'h3',
-	    null,
-	    'It is ',
+	    "h3",
+	    { className: "text-center" },
+	    "It is ",
 	    temp,
-	    ' in ',
+	    " in ",
 	    location
 	  );
 	};
